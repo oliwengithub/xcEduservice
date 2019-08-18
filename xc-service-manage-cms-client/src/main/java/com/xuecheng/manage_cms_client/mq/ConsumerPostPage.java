aeec6fb6b428f712133f29a9d072d92315f7bd39
178263682@qq.com <001518051406988>
2019-08-18 12:58:54 +0800

    添加了页面发布模块

---
+++ xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/mq/ConsumerPostPage.java
@@ -0,0 +1,35 @@
+package com.xuecheng.manage_cms_client.mq;
+
+import com.alibaba.fastjson.JSON;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.manage_cms_client.service.PageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 监听MQ，接收页面发布消息
+ * @author Administrator
+ * @version 1.0
+ **/
+@Component
+public class ConsumerPostPage {
+
+    @Autowired
+    PageService pageService;
+
+    @RabbitListener(queues = {"${xuecheng.mq.queue}"})
+    public void postPage(String msg){
+        //解析消息
+        Map map = JSON.parseObject(msg, Map.class);
+        //得到消息中的页面id
+        String pageId = (String) map.get("pageId");
+        //调用service方法将页面从GridFs中下载到服务器
+        pageService.savePageToServerPath(pageId);
+
+    }
+}
