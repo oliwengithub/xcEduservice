592ca660c422bf000466bb6e780b458e39bc2f94
178263682@qq.com <001518051406988>
2020-09-29 20:41:15 +0800

    添加媒体（视频）处理查询列表接口

---
+++ xc-service-api/src/main/java/com/xuecheng/api/media/ManageMediaFileControllerApi.java
@@ -0,0 +1,20 @@
+package com.xuecheng.api.media;
+
+import com.xuecheng.framework.domain.media.MediaFile;
+import com.xuecheng.framework.domain.media.request.QueryMediaFileRequest;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ *
+ * @author: olw
+ * @date: 2020/9/28 19:17
+ * @description:  媒资文件管理
+ */
+@Api(value = "媒资文件管理",description = "媒资文件管理",tags = {"媒资文件管理"})
+public interface ManageMediaFileControllerApi {
+
+    @ApiOperation(value = "查询所有文件列表")
+    public QueryResponseResult<MediaFile> findAll (Integer page, Integer size, QueryMediaFileRequest queryMediaFileRequest);
+}
